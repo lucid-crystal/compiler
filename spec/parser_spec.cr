1d46bd2ac3cf5509b2e147aadc42eba3f29566ff
devnote-dev <devnote.dev75@gmail.com>
2023-05-22 16:23:26 +0100

    feat(spec): add parser specs

---
+++ spec/parser_spec.cr
@@ -0,0 +1,35 @@
+require "./spec_helper"
+
+describe Compiler::Parser do
+  it "parses string expressions" do
+    assert_node_sequence([Compiler::StringLiteral], %("hello world"))
+  end
+
+  it "parses number-integer expressions" do
+    assert_node_sequence([Compiler::IntLiteral], %(123_45))
+  end
+
+  it "parses number-float expressions" do
+    assert_node_sequence([Compiler::FloatLiteral], %(3.141_592))
+  end
+
+  pending "parses nil expressions" do
+    assert_node_sequence([Compiler::NilLiteral], %(nil))
+  end
+
+  it "parses assignment expressions" do
+    assert_node_sequence([Compiler::Assign], %(x = 7))
+  end
+
+  it "parses variable declaration expresssions" do
+    assert_node_sequence([Compiler::Var], %(x : Int32))
+  end
+
+  it "parses call expressions with single arguments" do
+    assert_node_sequence([Compiler::Call], %(puts "hello world"))
+  end
+
+  it "parses call expressions with multiple arguments" do
+    assert_node_sequence([Compiler::Call], %(puts "foo", "bar", "baz"))
+  end
+end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -9,3 +9,12 @@ def assert_token_sequence(sequence : Array(Compiler::Token::Kind), for input : S
   kinds = Compiler::Lexer.new(input).run.map &.kind
   kinds.should eq sequence
 end
+
+def assert_node_sequence(sequence : Array(Compiler::Node.class), for input : String) : Nil
+  tokens = Compiler::Lexer.new(input).run
+  nodes = Compiler::Parser.new(tokens).parse
+
+  sequence.each_with_index do |cls, index|
+    nodes[index].class.should eq cls
+  end
+end
