d1cb315d556c5c011c989276582a021fcd0d633e
devnote-dev <devnote.dev75@gmail.com>
2024-11-20 23:14:56 +0000

    refactor(spec/parser): simplify tests & code quality

--- spec/parser/assign_and_vars_spec.cr
+++ spec/parser/assign_and_vars_spec.cr
@@ -3,43 +3,36 @@ require "../spec_helper"
 describe LC::Parser do
   context "assign and vars", tags: %w[parser assign vars] do
     it "parses assignment expressions" do
-      node = parse "x = 7"
-      node.should be_a LC::Assign
-      node = node.as(LC::Assign)
+      node = parse("x = 7").should be_a LC::Assign
+      target = node.target.should be_a LC::Ident
+      target.value.should eq "x"
 
-      node.target.should be_a LC::Ident
-      node.target.as(LC::Ident).value.should eq "x"
-
-      node.value.should be_a LC::IntLiteral
-      node.value.as(LC::IntLiteral).value.should eq 7
+      value = node.value.should be_a LC::IntLiteral
+      value.value.should eq 7
     end
 
     it "parses uninitialized variable declaration expressions" do
-      node = parse "x : Int32"
-      node.should be_a LC::Var
-      node = node.as(LC::Var)
+      node = parse("x : Int32").should be_a LC::Var
 
-      node.name.should be_a LC::Ident
-      node.name.as(LC::Ident).value.should eq "x"
+      name = node.name.should be_a LC::Ident
+      name.value.should eq "x"
       node.uninitialized?.should be_true
 
-      node.type.should be_a LC::Const
-      node.type.as(LC::Const).value.should eq "Int32"
+      type = node.type.should be_a LC::Const
+      type.value.should eq "Int32"
 
       node.value.should be_nil
     end
 
     it "parses initialized variable declaration expressions" do
-      node = parse "y : Int32 = 123"
-      node.should be_a LC::Var
-      node = node.as(LC::Var)
+      node = parse("y : Int32 = 123").should be_a LC::Var
 
-      node.name.should be_a LC::Ident
-      node.name.as(LC::Ident).value.should eq "y"
+      name = node.name.should be_a LC::Ident
+      name.value.should eq "y"
       node.uninitialized?.should be_false
 
-      node.type.should be_a LC::Const
-      node.type.as(LC::Const).value.should eq "Int32"
+      type = node.type.should be_a LC::Const
+      type.value.should eq "Int32"
 
       node.value.should be_a LC::IntLiteral
       node.value.as(LC::IntLiteral).value.should eq 123
--- spec/parser/calls_and_paths_spec.cr
+++ spec/parser/calls_and_paths_spec.cr
@@ -3,154 +3,127 @@ require "../spec_helper"
 describe LC::Parser do
   context "calls and paths", tags: %w[parser calls paths] do
     it "parses call expressions with no arguments" do
-      node = parse "exit"
-      node.should be_a LC::Call
-      node = node.as(LC::Call)
+      node = parse("exit").should be_a LC::Call
+      receiver = node.receiver.should be_a LC::Ident
 
-      node.receiver.should be_a LC::Ident
-      node.receiver.as(LC::Ident).value.should eq "exit"
+      receiver.value.should eq "exit"
       node.args.size.should eq 0
     end
 
     it "parses delimited call expressions" do
       {parse("puts;"), parse("puts\n")}.each do |node|
-        node.should be_a LC::Call
-        node = node.as(LC::Call)
-
-        node.receiver.should be_a LC::Ident
-        node.receiver.as(LC::Ident).value.should eq "puts"
+        node = node.should be_a LC::Call
+        receiver = node.receiver.should be_a LC::Ident
+        receiver.value.should eq "puts"
       end
     end
 
     it "parses path call expressions" do
-      node = parse "foo.bar.baz"
-      node.should be_a LC::Call
-      node = node.as(LC::Call)
+      node = parse("foo.bar.baz").should be_a LC::Call
+      receiver = node.receiver.should be_a LC::Path
+      receiver.names.size.should eq 3
 
-      node.receiver.should be_a LC::Path
-      names = node.receiver.as(LC::Path).names
+      ident = receiver.names[0].should be_a LC::Ident
+      ident.value.should eq "foo"
 
-      names.size.should eq 3
-      names[0].should be_a LC::Ident
-      names[0].as(LC::Ident).value.should eq "foo"
+      ident = receiver.names[1].should be_a LC::Ident
+      ident.value.should eq "bar"
 
-      names[1].should be_a LC::Ident
-      names[1].as(LC::Ident).value.should eq "bar"
-
-      names[2].should be_a LC::Ident
-      names[2].as(LC::Ident).value.should eq "baz"
+      ident = receiver.names[2].should be_a LC::Ident
+      ident.value.should eq "baz"
     end
 
     it "parses constant path expressions" do
-      node = parse "Foo::Bar"
-      node.should be_a LC::Path
-      node = node.as(LC::Path)
-
+      node = parse("Foo::Bar").should be_a LC::Path
       node.names.size.should eq 2
-      node.names[0].should be_a LC::Const
-      node.names[0].as(LC::Const).value.should eq "Foo"
-      node.names[0].as(LC::Const).global?.should be_false
 
-      node.names[1].should be_a LC::Const
-      node.names[1].as(LC::Const).value.should eq "Bar"
-      node.names[1].as(LC::Const).global?.should be_true
+      const = node.names[0].should be_a LC::Const
+      const.value.should eq "Foo"
+      const.global?.should be_false
+
+      const = node.names[1].should be_a LC::Const
+      const.value.should eq "Bar"
+      const.global?.should be_true
     end
 
     it "parses constant call expresions" do
-      node = parse "::Foo.baz"
-      node.should be_a LC::Call
-      node = node.as(LC::Call)
+      node = parse("::Foo.baz").should be_a LC::Call
+      receiver = node.receiver.should be_a LC::Path
+      receiver.names.size.should eq 2
 
-      node.receiver.should be_a LC::Path
-      names = node.receiver.as(LC::Path).names
+      const = receiver.names[0].should be_a LC::Const
+      const.value.should eq "Foo"
+      const.global?.should be_true
 
-      names.size.should eq 2
-      names[0].should be_a LC::Const
-      names[0].as(LC::Const).value.should eq "Foo"
-      names[0].as(LC::Const).global?.should be_true
-
-      names[1].should be_a LC::Ident
-      names[1].as(LC::Ident).value.should eq "baz"
-      names[1].as(LC::Ident).global?.should be_false
+      ident = receiver.names[1].should be_a LC::Ident
+      ident.value.should eq "baz"
+      ident.global?.should be_false
     end
 
     it "parses call expressions with single arguments" do
-      node = parse %(puts "hello world")
-      node.should be_a LC::Call
-      node = node.as(LC::Call)
-
-      node.receiver.should be_a LC::Ident
-      node.receiver.as(LC::Ident).value.should eq "puts"
-
+      node = parse(%(puts "hello world")).should be_a LC::Call
+      receiver = node.receiver.should be_a LC::Ident
+      receiver.value.should eq "puts"
       node.args.size.should eq 1
-      node.args[0].should be_a LC::StringLiteral
-      node.args[0].as(LC::StringLiteral).value.should eq "hello world"
+
+      str = node.args[0].should be_a LC::StringLiteral
+      str.value.should eq "hello world"
     end
 
     it "parses call expressions with multiple arguments" do
-      node = parse %(puts "foo", "bar", "baz")
-      node.should be_a LC::Call
-      node = node.as(LC::Call)
+      node = parse(%(puts "foo", "bar", "baz")).should be_a LC::Call
 
-      node.receiver.should be_a LC::Ident
-      node.receiver.as(LC::Ident).value.should eq "puts"
-
+      receiver = node.receiver.should be_a LC::Ident
+      receiver.value.should eq "puts"
       node.args.size.should eq 3
-      node.args[0].should be_a LC::StringLiteral
-      node.args[0].as(LC::StringLiteral).value.should eq "foo"
 
-      node.args[1].should be_a LC::StringLiteral
-      node.args[1].as(LC::StringLiteral).value.should eq "bar"
+      str = node.args[0].should be_a LC::StringLiteral
+      str.value.should eq "foo"
 
-      node.args[2].should be_a LC::StringLiteral
-      node.args[2].as(LC::StringLiteral).value.should eq "baz"
+      str = node.args[1].should be_a LC::StringLiteral
+      str.value.should eq "bar"
+
+      str = node.args[2].should be_a LC::StringLiteral
+      str.value.should eq "baz"
     end
 
     it "parses call expressions on multiple lines" do
-      node = parse <<-CR
+      node = parse(<<-CR).should be_a LC::Call
         puts(
           "hello from",
           "the other side",
         )
         CR
 
-      node.should be_a LC::Call
-      node = node.as(LC::Call)
-
-      node.receiver.should be_a LC::Ident
-      node.receiver.as(LC::Ident).value.should eq "puts"
-
+      receiver = node.receiver.should be_a LC::Ident
+      receiver.value.should eq "puts"
       node.args.size.should eq 2
-      node.args[0].should be_a LC::StringLiteral
-      node.args[0].as(LC::StringLiteral).value.should eq "hello from"
 
-      node.args[1].should be_a LC::StringLiteral
-      node.args[1].as(LC::StringLiteral).value.should eq "the other side"
+      str = node.args[0].should be_a LC::StringLiteral
+      str.value.should eq "hello from"
+
+      str = node.args[1].should be_a LC::StringLiteral
+      str.value.should eq "the other side"
     end
 
     it "parses nested call expressions" do
-      node = parse <<-CR
+      node = parse(<<-CR).should be_a LC::Call
         puts(
           "hello, ",
           your_name,
         )
         CR
 
-      node.should be_a LC::Call
-      node = node.as(LC::Call)
-
-      node.receiver.should be_a LC::Ident
-      node.receiver.as(LC::Ident).value.should eq "puts"
-
+      receiver = node.receiver.should be_a LC::Ident
+      receiver.value.should eq "puts"
       node.args.size.should eq 2
-      node.args[0].should be_a LC::StringLiteral
-      node.args[0].as(LC::StringLiteral).value.should eq "hello, "
 
-      node.args[1].should be_a LC::Call
-      inner = node.args[1].as(LC::Call)
+      str = node.args[0].should be_a LC::StringLiteral
+      str.value.should eq "hello, "
 
-      inner.receiver.should be_a LC::Ident
-      inner.receiver.as(LC::Ident).value.should eq "your_name"
+      call = node.args[1].should be_a LC::Call
+      receiver = call.receiver.should be_a LC::Ident
+      receiver.value.should eq "your_name"
     end
 
     # TODO: use refined exceptions for these
@@ -168,182 +141,142 @@ describe LC::Parser do
     end
 
     it "parses call expressions with a single variable declaration" do
-      node = parse "::property(name : String)"
-
-      node.should be_a LC::Call
-      node = node.as(LC::Call)
-
-      node.receiver.should be_a LC::Ident
-      node.receiver.as(LC::Ident).value.should eq "property"
-      node.receiver.as(LC::Ident).global?.should be_true
-
+      node = parse("::property(name : String)").should be_a LC::Call
+      receiver = node.receiver.should be_a LC::Ident
+      receiver.value.should eq "property"
+      receiver.global?.should be_true
       node.args.size.should eq 1
-      node.args[0].should be_a LC::Var
-      var = node.args[0].as(LC::Var)
 
-      var.name.should be_a LC::Ident
-      var.name.as(LC::Ident).value.should eq "name"
+      var = node.args[0].should be_a LC::Var
+      name = var.name.should be_a LC::Ident
+      name.value.should eq "name"
 
-      var.type.should be_a LC::Const
-      var.type.as(LC::Const).value.should eq "String"
+      const = var.type.should be_a LC::Const
+      const.value.should eq "String"
       var.value.should be_nil
     end
 
     it "parses call expressions with a single variable assignment" do
-      node = parse %(::property(name = "dev"))
-
-      node.should be_a LC::Call
-      node = node.as(LC::Call)
-
-      node.receiver.should be_a LC::Ident
-      node.receiver.as(LC::Ident).value.should eq "property"
-      node.receiver.as(LC::Ident).global?.should be_true
-
+      node = parse(%(::property(name = "dev"))).should be_a LC::Call
+      receiver = node.receiver.should be_a LC::Ident
+      receiver.value.should eq "property"
+      receiver.global?.should be_true
       node.args.size.should eq 1
-      node.args[0].should be_a LC::Assign
-      var = node.args[0].as(LC::Assign)
 
-      var.target.should be_a LC::Ident
-      var.target.as(LC::Ident).value.should eq "name"
+      var = node.args[0].should be_a LC::Assign
+      ident = var.target.should be_a LC::Ident
+      ident.value.should eq "name"
 
-      var.value.should be_a LC::StringLiteral
-      var.value.as(LC::StringLiteral).value.should eq "dev"
+      str = var.value.should be_a LC::StringLiteral
+      str.value.should eq "dev"
     end
 
     it "parses call expressions with a single variable declaration and assignment" do
-      node = parse %(::property(name : String = "dev"))
-
-      node.should be_a LC::Call
-      node = node.as(LC::Call)
-
-      node.receiver.should be_a LC::Ident
-      node.receiver.as(LC::Ident).value.should eq "property"
-      node.receiver.as(LC::Ident).global?.should be_true
-
+      node = parse(%(::property(name : String = "dev"))).should be_a LC::Call
+      receiver = node.receiver.should be_a LC::Ident
+      receiver.value.should eq "property"
+      receiver.global?.should be_true
       node.args.size.should eq 1
-      node.args[0].should be_a LC::Var
-      var = node.args[0].as(LC::Var)
 
-      var.name.should be_a LC::Ident
-      var.name.as(LC::Ident).value.should eq "name"
+      var = node.args[0].should be_a LC::Var
+      name = var.name.should be_a LC::Ident
+      name.value.should eq "name"
 
-      var.type.should be_a LC::Const
-      var.type.as(LC::Const).value.should eq "String"
+      const = var.type.should be_a LC::Const
+      const.value.should eq "String"
 
-      var.value.should be_a LC::StringLiteral
-      var.value.as(LC::StringLiteral).value.should eq "dev"
+      str = var.value.should be_a LC::StringLiteral
+      str.value.should eq "dev"
     end
 
     it "parses call expressions with multiple variable declarations" do
-      node = parse "record Foo, bar : Int32, baz : String"
-
-      node.should be_a LC::Call
-      node = node.as(LC::Call)
-
-      node.receiver.should be_a LC::Ident
-      node.receiver.as(LC::Ident).value.should eq "record"
-
+      node = parse("record Foo, bar : Int32, baz : String").should be_a LC::Call
+      receiver = node.receiver.should be_a LC::Ident
+      receiver.value.should eq "record"
       node.args.size.should eq 3
-      node.args[0].should be_a LC::Const
-      node.args[0].as(LC::Const).value.should eq "Foo"
 
-      node.args[1].should be_a LC::Var
-      var = node.args[1].as(LC::Var)
-      var.name.should be_a LC::Ident
-      var.name.as(LC::Ident).value.should eq "bar"
+      const = node.args[0].should be_a LC::Const
+      const.value.should eq "Foo"
 
-      var.type.should be_a LC::Const
-      var.type.as(LC::Const).value.should eq "Int32"
+      var = node.args[1].should be_a LC::Var
+      name = var.name.should be_a LC::Ident
+      name.value.should eq "bar"
+
+      const = var.type.should be_a LC::Const
+      const.value.should eq "Int32"
       var.value.should be_nil
 
-      node.args[2].should be_a LC::Var
-      var = node.args[2].as(LC::Var)
-      var.name.should be_a LC::Ident
-      var.name.as(LC::Ident).value.should eq "baz"
+      var = node.args[2].should be_a LC::Var
+      name = var.name.should be_a LC::Ident
+      name.value.should eq "baz"
 
-      var.type.should be_a LC::Const
-      var.type.as(LC::Const).value.should eq "String"
+      const = var.type.should be_a LC::Const
+      const.value.should eq "String"
       var.value.should be_nil
     end
 
     it "parses call expressions with multiple variable assignments" do
-      node = parse %(record Foo, bar = 123, baz = "true")
-
-      node.should be_a LC::Call
-      node = node.as(LC::Call)
-
-      node.receiver.should be_a LC::Ident
-      node.receiver.as(LC::Ident).value.should eq "record"
-
+      node = parse(%(record Foo, bar = 123, baz = "true")).should be_a LC::Call
+      receiver = node.receiver.should be_a LC::Ident
+      receiver.value.should eq "record"
       node.args.size.should eq 3
-      node.args[0].should be_a LC::Const
-      node.args[0].as(LC::Const).value.should eq "Foo"
 
-      node.args[1].should be_a LC::Assign
-      var = node.args[1].as(LC::Assign)
-      var.target.should be_a LC::Ident
-      var.target.as(LC::Ident).value.should eq "bar"
+      const = node.args[0].should be_a LC::Const
+      const.value.should eq "Foo"
 
-      var.value.should be_a LC::IntLiteral
-      var.value.as(LC::IntLiteral).value.should eq 123
+      var = node.args[1].should be_a LC::Assign
+      ident = var.target.should be_a LC::Ident
+      ident.value.should eq "bar"
 
-      node.args[2].should be_a LC::Assign
-      var = node.args[2].as(LC::Assign)
-      var.target.should be_a LC::Ident
-      var.target.as(LC::Ident).value.should eq "baz"
+      int = var.value.should be_a LC::IntLiteral
+      int.value.should eq 123
 
-      var.value.should be_a LC::StringLiteral
-      var.value.as(LC::StringLiteral).value.should eq "true"
+      var = node.args[2].should be_a LC::Assign
+      ident = var.target.should be_a LC::Ident
+      ident.value.should eq "baz"
+
+      str = var.value.should be_a LC::StringLiteral
+      str.value.should eq "true"
     end
 
     it "parses call expressions with multiple variable declarations and assignments" do
-      node = parse %(record Foo, bar : Int32 = 123, baz : String = "true")
-
-      node.should be_a LC::Call
-      node = node.as(LC::Call)
-
-      node.receiver.should be_a LC::Ident
-      node.receiver.as(LC::Ident).value.should eq "record"
-
+      node = parse(%(record Foo, bar : Int32 = 123, baz : String = "true")).should be_a LC::Call
+      receiver = node.receiver.should be_a LC::Ident
+      receiver.value.should eq "record"
       node.args.size.should eq 3
-      node.args[0].should be_a LC::Const
-      node.args[0].as(LC::Const).value.should eq "Foo"
 
-      node.args[1].should be_a LC::Var
-      var = node.args[1].as(LC::Var)
-      var.name.should be_a LC::Ident
-      var.name.as(LC::Ident).value.should eq "bar"
+      const = node.args[0].should be_a LC::Const
+      const.value.should eq "Foo"
 
-      var.type.should be_a LC::Const
-      var.type.as(LC::Const).value.should eq "Int32"
+      var = node.args[1].should be_a LC::Var
+      ident = var.name.should be_a LC::Ident
+      ident.value.should eq "bar"
 
-      var.value.should be_a LC::IntLiteral
-      var.value.as(LC::IntLiteral).value.should eq 123
+      const = var.type.should be_a LC::Const
+      const.value.should eq "Int32"
 
-      node.args[2].should be_a LC::Var
-      var = node.args[2].as(LC::Var)
-      var.name.should be_a LC::Ident
-      var.name.as(LC::Ident).value.should eq "baz"
+      int = var.value.should be_a LC::IntLiteral
+      int.value.should eq 123
 
-      var.type.should be_a LC::Const
-      var.type.as(LC::Const).value.should eq "String"
+      var = node.args[2].should be_a LC::Var
+      ident = var.name.should be_a LC::Ident
+      ident.value.should eq "baz"
 
-      var.value.should be_a LC::StringLiteral
-      var.value.as(LC::StringLiteral).value.should eq "true"
+      const = var.type.should be_a LC::Const
+      const.value.should eq "String"
+
+      str = var.value.should be_a LC::StringLiteral
+      str.value.should eq "true"
     end
 
     it "parses expressions ignoring semicolons" do
-      node = parse %(;;;;;;;puts "hello world";;;;;;;)
-
-      node.should be_a LC::Call
-      node = node.as(LC::Call)
-
-      node.receiver.should be_a LC::Ident
-      node.receiver.as(LC::Ident).value.should eq "puts"
-
+      node = parse(%(;;;;;;;puts "hello world";;;;;;;)).should be_a LC::Call
+      receiver = node.receiver.should be_a LC::Ident
+      receiver.value.should eq "puts"
       node.args.size.should eq 1
-      node.args[0].should be_a LC::StringLiteral
-      node.args[0].as(LC::StringLiteral).value.should eq "hello world"
+
+      str = node.args[0].should be_a LC::StringLiteral
+      str.value.should eq "hello world"
     end
   end
 end
