4a173e0bb18c4606355a3c096e412dd9214c6629
devnote-dev <devnote.dev75@gmail.com>
2024-12-01 00:19:45 +0000

    feat(spec/parser): path/call coverage for error tokens/nodes

--- spec/parser/calls_and_paths_spec.cr
+++ spec/parser/calls_and_paths_spec.cr
@@ -123,6 +123,59 @@ describe LC::Parser do
       ident.value.should eq "end"
     end
 
+    it "parses invalid calls as errors" do
+      call = parse(%(foo."bar")).should be_a LC::Call
+      path = call.receiver.should be_a LC::Path
+      path.names.size.should eq 2
+
+      ident = path.names[0].should be_a LC::Ident
+      ident.value.should eq "foo"
+
+      error = path.names[1].should be_a LC::Error
+      token = error.target.should be_a LC::Token
+      token.kind.string?.should be_true
+      token.raw_value.should eq "bar"
+      error.message.should eq %(unexpected token "bar")
+
+      call = parse("Foo::'a'::Bar").should be_a LC::Call
+      path = call.receiver.should be_a LC::Path
+      path.names.size.should eq 3
+
+      const = path.names[0].should be_a LC::Const
+      const.value.should eq "Foo"
+
+      error = path.names[1].should be_a LC::Error
+      token = error.target.should be_a LC::Token
+      token.kind.char?.should be_true
+      token.raw_value.should eq 'a'
+      error.message.should eq %(unexpected token 'a')
+
+      const = path.names[2].should be_a LC::Const
+      const.value.should eq "Bar"
+
+      call = parse("Foo.bar::Baz").should be_a LC::Call
+      path = call.receiver.should be_a LC::Path
+      path.names.size.should eq 3
+
+      const = path.names[0].should be_a LC::Const
+      const.value.should eq "Foo"
+
+      ident = path.names[1].should be_a LC::Ident
+      ident.value.should eq "bar"
+
+      error = path.names[2].should be_a LC::Error
+      token = error.target.should be_a LC::Token
+      token.kind.double_colon?.should be_true
+      token.raw_value.should be_nil
+      error.message.should eq "unexpected token 'double_colon'"
+
+      error = path.names[3].should be_a LC::Error
+      const = error.target.should be_a LC::Const
+      const.value.should eq "Baz"
+      const.global?.should be_true
+      error.message.should eq %(unexpected token "Baz")
+    end
+
     it "parses call expressions with single arguments" do
       call = parse(%(puts "hello world")).should be_a LC::Call
       ident = call.receiver.should be_a LC::Ident
@@ -189,12 +242,19 @@ describe LC::Parser do
       ident.value.should eq "your_name"
     end
 
-    # TODO: use refined exceptions for these
+    # TODO: also test fail-first exceptions here
 
-    it "raises on undelimited arguments for calls" do
-      expect_raises(Exception, "expected a comma after the last argument") do
-        parse %(puts "foo" "bar")
-      end
+    it "parses undelimited call arguments" do
+      call = parse(%(puts "foo" "bar")).should be_a LC::Call
+      call.args.size.should eq 2
+
+      str = call.args[0].should be_a LC::StringLiteral
+      str.value.should eq "foo"
+
+      error = call.args[1].should be_a LC::Error
+      str = error.target.should be_a LC::StringLiteral
+      str.value.should eq "bar"
+      error.message.should eq "expected a comma after the last argument"
     end
 
     it "raises on unclosed parentheses for calls" do
