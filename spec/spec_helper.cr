ffbe2023490eed1f8b05dcde4929b69e30b3cd0f
devnote-dev <devnote.dev75@gmail.com>
2025-01-11 19:21:35 +0000

    refactor(spec/lexer): rewrite most specs to include location tests

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -17,6 +17,27 @@ def assert_node(cls : LC::Node.class, for input : String) : Nil
   parse(input).class.should eq cls
 end
 
+# TODO: remove this
 def assert_tokens(source : String, *kinds : LC::Token::Kind) : Nil
   LC::Lexer.run(source).map(&.kind).should eq kinds.to_a
+end
+
+def assert_tokens(source : String, *pairs : {LC::Token::Kind, Int32, Int32, Int32, Int32}) : Nil
+  tokens = LC::Lexer.run source
+  pairs.each_with_index do |(kind, *loc), index|
+    token = tokens[index]
+    token.kind.should eq kind
+    token.loc.to_tuple.should eq loc
+  end
+end
+
+macro t!(s)
+  {%
+    if s.stringify == "eof"
+      id = s.id.upcase.id
+    else
+      id = s.id.camelcase.id
+    end
+  %}
+  ::Lucid::Compiler::Token::Kind::{{id}}
 end
